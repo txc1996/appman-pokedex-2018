89104bdea210caaa7d541526918637e5ab5205b9
Cuong Tran <cuong.tran@gpu.exchange>
2019-12-18 19:33:53 +0700

    Add remove and add pokemon feature

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -12,6 +12,44 @@ class PokeCard extends React.Component {
     }
   }
 
+  caculateHP = (input) => {
+    if (isNaN(input)) {
+      return 0
+    }
+    return parseInt(input) >= 100 ? 100 : input
+  }
+
+  caculateStr = (attacks) => {
+    if (!Array.isArray(attacks)) {
+      return 0
+    }
+    const point = attacks.length * 50
+    return point >= 100 ? 100 : point
+  }
+
+  caculateWeakness = (weaknesses) => {
+    if (!Array.isArray(weaknesses)) {
+      return 0
+    }
+    const point = weaknesses.length * 100
+    return point === 100 ? 100 : 0
+  }
+
+  caculateDamage = (attacks) => {
+    if (!Array.isArray(attacks)) {
+      return 0
+    }
+    return  attacks.reduce((total, attack) => {
+      const damage = parseInt(attack.damage.replace(/\D+/g, ''))
+      return total + (isNaN(damage) ? 0 : damage)
+    }, 0)
+  }
+
+  caculateHappyness = (hp, damage, weakness) => {
+    const point = ((hp / 10) + (damage /10 ) + 10 - (weakness / 100)) / 5
+    return point
+  }
+
   render() {
     return (
       <Styles.Container 
@@ -19,15 +57,24 @@ class PokeCard extends React.Component {
         onMouseOver={() => this.setState({ hover: true })}
         onMouseOut={() => this.setState({ hover: false })}
       >
-        <Styles.Image src="https://images.pokemontcg.io/ex8/98.png"></Styles.Image>
+        <Styles.Image src={this.props.card.imageUrl}></Styles.Image>
         <Styles.Info>
-          <Styles.Name>deoxys ex</Styles.Name>
-          <Styles.Progress max="100" value="80" title="hp"></Styles.Progress>
-          <Styles.Progress max="100" value="80" title="str"></Styles.Progress>
-          <Styles.Progress max="100" value="80" title="weak"></Styles.Progress>
-          <Stars number="5"></Stars>
+        <Styles.Name>{this.props.card.name}</Styles.Name>
+          <Styles.Progress max="100" value={this.caculateHP(this.props.card.hp)} title="hp" />
+          <Styles.Progress max="100" value={this.caculateStr(this.props.card.attacks)} title="str"></Styles.Progress>
+          <Styles.Progress max="100" value={this.caculateWeakness(this.props.card.weaknesses)} title="weak"></Styles.Progress>
+          <Stars number={this.caculateHappyness(
+            this.caculateHP(this.props.card.hp),
+            this.caculateDamage(this.props.card.attacks),
+            this.caculateWeakness(this.props.card.weaknesses)
+          )}>
+          </Stars>
         </Styles.Info>
-        <Styles.Action display={this.state.hover} actionTitle={this.props.actionTitle}></Styles.Action>
+        <Styles.Action 
+          display={this.state.hover}
+          actionTitle={this.props.actionTitle}
+          onClick={this.props.onAction}
+        ></Styles.Action>
       </Styles.Container>
     )
   }
--- src/pages/MyPokedex/index.js
+++ src/pages/MyPokedex/index.js
@@ -10,15 +10,67 @@ class MyPokedek extends React.Component {
 
     this.state = {
       openListPoke: false,
+      myListPoke: [],
+      searchPoke: []
     }
   }
+
+  componentDidMount() {
+    fetch("http://localhost:3030/api/cards")
+    .then(response => response.json())
+    .then(cards => this.setState({ searchPoke: cards.cards }))
+  }
+
+  removeCard = (card) => {
+    const newSearchList = [...this.state.searchPoke]
+    newSearchList.push(card)
+    this.setState({
+      myListPoke: [...this.state.myListPoke].filter((item) => {
+        return item.id !== card.id
+      }),
+      searchPoke: newSearchList
+    })
+  }
+
+  addCard = (card) => {
+    const myNewPokeList = [...this.state.myListPoke]
+    myNewPokeList.push(card)
+    this.setState({
+      myListPoke: myNewPokeList,
+      searchPoke: [...this.state.searchPoke].filter((item) => {
+        return item.id !== card.id
+      })
+    })
+  }
+
+  renderMyListPoke = () => {
+    return this.state.myListPoke.map((card) => (
+      <PokeCard
+        actionTitle="x"
+        key={card.id}
+        card={card}
+        onAction={() => this.removeCard(card)}
+      ></PokeCard>
+    ))
+  }
+
+  renderSearchPoke = () => {
+    return this.state.searchPoke.map((card) => (
+      <PokeCard
+        style={{width: "95%"}}
+        actionTitle="add"
+        card={card}
+        key={card.id}
+        onAction={() => this.addCard(card)}/>
+    ))
+  }
+
   render() {
     return (
       <Styles.Container>
         <Styles.Header>My Pokedex</Styles.Header>
         <Styles.ListCardContainer>
-          <PokeCard actionTitle="x"></PokeCard>
-          <PokeCard actionTitle="x"></PokeCard>
+          {this.renderMyListPoke()}
         </Styles.ListCardContainer>
         <Styles.Footer>
           <Styles.AddButton 
@@ -33,9 +85,7 @@ class MyPokedek extends React.Component {
         >
           <Styles.PopupContainer>
             <Styles.Search placeholder="Find Pokemon"></Styles.Search>
-            <PokeCard style={{width: "95%"}} actionTitle="add"></PokeCard>
-            <PokeCard style={{width: "95%"}} actionTitle="add"></PokeCard>
-            <PokeCard style={{width: "95%"}} actionTitle="add"></PokeCard>
+            {this.renderSearchPoke()}
           </Styles.PopupContainer>
         </Popup>
       </Styles.Container>
